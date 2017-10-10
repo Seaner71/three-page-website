db5b1a1775261d17df9eea2fe0d2a90c8c4ae9ae
echo 'hello world <ssmyth1071@gmail.com>
2017-10-10 17:35:33 -0400

    add  empty ul and li elements onclick

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,22 @@
 function addButton() {
-  document.getElementById('name').innerHTML ="Sean Smyth";
+  // document.getElementById('name').innerHTML ="Sean Smyth";
   document.getElementById('job').innerHTML ="Web Developer";
   document.getElementById('experience').innerHTML ='Financial Professional';
 }
+
+function onLoad() {
+  document.getElementById('name').innerHTML = "Sean Smyth"
+}
+
+var skillsArray = ['HTML', "CSS", "JavaScript","Ruby", "SQL"]
+
+function addSkills() {
+  var ul = document.createElement('ul');
+  document.getElementById('home-text').appendChild(ul);
+  document.getElementById('home-text').appendChild(document.createElement('li'));
+
+  // document.write("hello")
+}
+
+
+// document.createElement('<ul><li>' + skillsArray.join('</li><li>') + '</li></ul>')
