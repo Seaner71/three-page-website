04f09c04be4e6beb8742a33ede4cc22e3f33ab41
echo 'hello world <ssmyth1071@gmail.com>
2017-10-10 21:14:34 -0400

    remove unecessary class from header links

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
+
+
+
 function addButton() {
-  // document.getElementById('name').innerHTML ="Sean Smyth";
+  document.getElementById('name').innerHTML ="Sean Smyth";
   document.getElementById('job').innerHTML ="Web Developer";
   document.getElementById('experience').innerHTML ='Financial Professional';
 }
@@ -13,7 +16,7 @@ function addSkills() {
       var ul = document.createElement('ul');
       ul.setAttribute('id','skillList');
 
-      var t, tt;
+      var t;
       skillsArray = ['HTML', "CSS", "JavaScript","Ruby", "SQL","React"]
       document.getElementById('home-text').appendChild(ul);
       skillsArray.forEach(renderSkillsArray);
